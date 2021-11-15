61b2c14708438c2d837920f56c27498d1af909f0
LAPTOP-Q4E3DFIH\s9633 <s3a832070@student.ncut.edu.tw>
2021-11-15 21:22:46 +0800

    第二個路由：新增任務與資料驗證

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,21 @@ Route::get('/', function () {
 });
 // 增加新的任務
 Route::post('/task', function (Request $request) {
-    //
+    $validator = Validator::make($request->all() , [
+        'name' => 'required|max:255',
+    ]);
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+    // 建立該任務...
+//新增任務存入DB的程式碼 (see next page)
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
 });
 // 刪除任務
 Route::delete('/task/{task}', function (Task $task) {
