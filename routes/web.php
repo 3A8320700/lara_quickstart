0408b32ae611da829c5646a10764587bdf8fa358
LAPTOP-Q4E3DFIH\s9633 <s3a832070@student.ncut.edu.tw>
2021-11-15 21:18:47 +0800

    建置第一個路由

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,16 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 //顯示所有任務的清單
 Route::get('/', function () {
-    //
+    return view('tasks');
 });
 // 增加新的任務
 Route::post('/task', function (Request $request) {
     //
 });
 // 刪除任務
-Route::delete('/task/{task}
+Route::delete('/task/{task}', function (Task $task) {
+    //
+});
