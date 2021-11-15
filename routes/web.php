143ed7371d29d421dd7ddc33c0d49f8d0e2a43ab
LAPTOP-Q4E3DFIH\s9633 <s3a832070@student.ncut.edu.tw>
2021-11-15 21:09:55 +0800

    建置三個路由(Routes)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Task;
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//顯示所有任務的清單
+Route::get('/', function () {
+    //
+});
+// 增加新的任務
+Route::post('/task', function (Request $request) {
+    //
+});
+// 刪除任務
+Route::delete('/task/{task}
